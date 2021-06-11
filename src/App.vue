f6f8c7c0c1660ecc74bbb50622644147c1298573
vikas339 <75200810+vikas339@users.noreply.github.com>
2021-06-11 12:15:39 +0530

    baselayout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<router-view>
+  <nav id="navbar-main" aria-label="Primary navigation" class="navbar navbar-main navbar-expand-lg navbar-theme-primary headroom navbar-light navbar-transparent headroom--not-top headroom--not-bottom headroom--pinned">
+   <div class="d-flex align-items-center">
+    <button type="submit" class="btn btn-primary ml-2"> <router-link to="/">Home</router-link></button>
+    <button type="submit" class="btn btn-primary ml-2"> <router-link to="/register">Register</router-link></button>
+    <button type="submit" class="btn btn-primary ml-2"> <router-link to="/baseLayout">Baselayout</router-link></button>
+    
+   </div>     
+  </nav>
+<router-view/>
 </template>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Baselayout from '../views/Baselayout.vue'
 
 const routes = [
   {
@@ -7,15 +8,21 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
+  // {
+  //   path: '/about',
+  //   name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    // component: function () {
+      // return import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //   }
+  // },
+  {
+    path: '/baselayout',
+    name: 'Baselayout',
+    component:Baselayout 
+
   }
 ]
 
