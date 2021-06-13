4216bd44f99eb5bee8a936455d42b7b9a1b68033
Dhritesh Bhagat <dhriteshbhagat@gmail.com>
2021-06-13 23:38:44 +0530

    updated by making the logo an svg code

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,88 +1,137 @@
 <template>
+  <div class="box" style="text-align: center;">
+    <div class="mb-5">
+      <!-- Generator: Adobe Illustrator 25.0.1, SVG Export Plug-In  -->
+      <svg class="mt-3"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        width="200px"
+        height="200px"
+        viewBox="0 0 225.01 243.51"
+        style="overflow:visible;enable-background:new 0 0 225.01 243.51;"
+        xml:space="preserve"
+        color="#000000"
+      >
+        <defs></defs>
+        <g>
+          <path
+            d="M137.52,0c24.9,10.26,48.5,22.03,65.41,44.48c31.07,41.23,29.3,102.02-5.02,140.79c-2.92,3.3-6.49,6.03-11.29,10.42
+		c-5.56-30.23-10.93-58.44-15.9-86.73c-6.15-35.05-26.09-53.37-57.4-54.7c-26.47-1.12-51.46,15.52-58.52,51.53
+		c-2.88,14.7-4.53,29.64-7.34,44.35c-2.7,14.17-6.18,28.2-9.62,43.68c-14.18-8-21.92-21.55-27.93-35.87
+		C-16.85,94.26,12.16,29.15,77.87,4.43c3.32-1.25,6.47-2.95,9.7-4.43C104.22,0,120.87,0,137.52,0z"
+          />
+          <path
+            d="M75.08,243.51c-2.48-5.68-4.96-11.36-8.24-18.84c31.58,0,59.81,0,91.39,0c-3.27,7.5-5.75,13.17-8.23,18.84
+		C125.03,243.51,100.06,243.51,75.08,243.51z"
+          />
+          <path
+            d="M69.44,97.74c30.07,0,57.45,0,86.19,0c6.45,25.06,12.84,49.85,19.61,76.14c-41.94,0-82.49,0-125.31,0
+		C56.5,148.21,62.93,123.16,69.44,97.74z"
+          />
+          <path
+            d="M173.81,185.6c4.6,23.5,0.34,29.19-21.83,29.26c-25.92,0.08-51.85,0.06-77.77,0.01c-23.13-0.05-26.49-4.09-23.63-29.27
+		C91.58,185.6,132.73,185.6,173.81,185.6z"
+          />
+        </g>
+      </svg>
 
-    <div class="box" style="text-align: center;">
-     <div class="mb-5">
-       <img class="mb-2" src="https://user-images.githubusercontent.com/75200810/121477814-edd84700-c9e5-11eb-898c-a21b4d8038e4.png" alt="Rozprava" width="200px">
-     
-     <h1 style="margin: 0%; margin-top: 0.5rem; font-size:3rem">ROZPRAVA</h1>
-     <h3 style="margin: 0%; margin-top: 0rem; margin-bottom: 2rem; font-size: 1.2rem;">Express yourself</h3>
+      <h1 style="margin: 0%; margin-top: 0.5rem; font-size:3rem">ROZPRAVA</h1>
+      <h3
+        style="margin: 0%; margin-top: 0rem; margin-bottom: 2rem; font-size: 1.2rem;"
+      >
+        Express yourself
+      </h3>
     </div>
-  
-  <div class="row justify-content-md-around">
-           <div class="col-lg-4 col-md-6">
-                        <!-- Button Social -->
-                        <button class="btn btn-pill github mb-3 pad-email" type="button">
-                            <span class="mr-1">
-                            Sign up using Email
-                            <img class="" src="https://user-images.githubusercontent.com/83393749/121353745-d3518f80-c94b-11eb-9fab-9b68f81383ed.png">
-                            </span>
-                        </button>
-                        <br>
-                        <button class="btn btn-pill github mb-3 pad-twitter" type="button">
-                            <span class="mr-1">
-                            Sign up using Twitter
-                            <img class="" src="https://user-images.githubusercontent.com/83393749/121353888-f9772f80-c94b-11eb-9776-b7f4a5e003b5.png"></span>
-                        </button>
-                        <br>
-                        <button class="btn btn-pill github mb-3 pad-google" type="button">
-                            <span class="mr-1">
-                            Sign up using Google
-                            <img class="" src="https://user-images.githubusercontent.com/83393749/121353904-fd0ab680-c94b-11eb-8c81-a4b8ffac5a14.png"></span>
-                        </button>
-                        <br>
-                        <button class="btn btn-pill github mb-3 pad-facebook" type="button">
-                            <span class="mr-1">
-                            Sign up using Facebook
-                            <img class="" src="https://user-images.githubusercontent.com/83393749/121784049-4b7bb780-cbcf-11eb-913a-c47cc60d7831.png"></span>
-                        </button>
-                        <div class="mb-4 mt-5" >
-                           
-                            <h3 class="h5 font-weight-bold">--------------- OR ----------------</h3>
-                            
-                        </div>
-                        <button class="btn btn-pill btn-primary login" type="button">Login</button>
-                    </div>
-                </div> 
-        </div>               
 
+    <div class="row justify-content-md-around">
+      <div class="col-lg-4 col-md-6">
+        <!-- Button Social -->
+        <button class="btn btn-pill github mb-3 pad-email" type="button">
+          <span class="mr-1">
+            Sign up using Email
+            <img
+              class=""
+              src="https://user-images.githubusercontent.com/83393749/121353745-d3518f80-c94b-11eb-9fab-9b68f81383ed.png"
+            />
+          </span>
+        </button>
+        <br />
+        <button class="btn btn-pill github mb-3 pad-twitter" type="button">
+          <span class="mr-1">
+            Sign up using Twitter
+            <img
+              class=""
+              src="https://user-images.githubusercontent.com/83393749/121353888-f9772f80-c94b-11eb-9776-b7f4a5e003b5.png"
+          /></span>
+        </button>
+        <br />
+        <button class="btn btn-pill github mb-3 pad-google" type="button">
+          <span class="mr-1">
+            Sign up using Google
+            <img
+              class=""
+              src="https://user-images.githubusercontent.com/83393749/121353904-fd0ab680-c94b-11eb-8c81-a4b8ffac5a14.png"
+          /></span>
+        </button>
+        <br />
+        <button class="btn btn-pill github mb-3 pad-facebook" type="button">
+          <span class="mr-1">
+            Sign up using Facebook
+            <img
+              class=""
+              src="https://user-images.githubusercontent.com/83393749/121784049-4b7bb780-cbcf-11eb-913a-c47cc60d7831.png"
+          /></span>
+        </button>
+        <div class="mb-4 mt-5">
+          <h3 class="h5 font-weight-bold">
+            --------------- OR ----------------
+          </h3>
+        </div>
+        <button class="btn btn-pill btn-primary login" type="button">
+          Login
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'SignUp',
-  
-}
+  name: "SignUp",
+};
 </script>
 
 <style scoped>
-.box{
+.box {
   font-family: Josefin Sans;
   margin-top: 5rem;
   padding: 0%;
 }
 .login {
-    padding-left: 8.6rem;
-    padding-right: 8.6rem;
+  padding-left: 8.6rem;
+  padding-right: 8.6rem;
 }
 
 .pad-email {
-    padding-left: 4.2rem;
-    padding-right: 4.2rem;
+  padding-left: 4.2rem;
+  padding-right: 4.2rem;
 }
 
 .pad-twitter {
-    padding-left: 4rem;
-    padding-right: 4rem;
+  padding-left: 4rem;
+  padding-right: 4rem;
 }
 
 .pad-google {
-    padding-left: 4rem;
-    padding-right: 4rem;
+  padding-left: 4rem;
+  padding-right: 4rem;
 }
 
 .pad-facebook {
-    padding-left: 3.4rem;
-    padding-right: 3.4rem;
+  padding-left: 3.4rem;
+  padding-right: 3.4rem;
 }
 </style>
