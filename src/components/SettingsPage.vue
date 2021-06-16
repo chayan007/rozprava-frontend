de897edbba1330091fd9394685f74b5e164a4f70
Dhritesh Bhagat <dhriteshbhagat@gmail.com>
2021-06-16 16:49:29 +0530

    finished the settings page

--- src/components/SettingsPage.vue
+++ src/components/SettingsPage.vue
@@ -1,61 +1,4 @@
 <template>
-  <!--<div style="text-align: center;">
-    <h3 style="margin-top: 7rem; font-size: 2rem;">Settings</h3>
-  
-  <div class="mt-4">
-                
-                <h3 style="font-size: 1.3rem; margin-right:7rem;">Change Profile Picture</h3>
-                <h3 style="font-size: 1.3rem; margin-right:13rem;">Change BIO</h3>
-                <h3 style="font-size: 1.3rem; margin-right:5.3rem;">Change <a href="#" style="color: green;">Profile Password</a></h3>
-                <span style="display: inline-flex;">
-                  <h3 style="font-size: 1.3rem; margin-right: 8.8rem; margin-left: 1rem;">Social Link</h3>
-                  <div style="">
-                    <img src="https://user-images.githubusercontent.com/83393749/121353904-fd0ab680-c94b-11eb-8c81-a4b8ffac5a14.png"/> 
-                    <img src="https://user-images.githubusercontent.com/83393749/121784049-4b7bb780-cbcf-11eb-913a-c47cc60d7831.png"/>
-                    <img src="https://user-images.githubusercontent.com/83393749/121353888-f9772f80-c94b-11eb-9776-b7f4a5e003b5.png"/> 
-                  </div>
-                </span>
-                <h3 style="font-size: 1.3rem; margin-right:9rem;">Change Username</h3>
-                <span style="display: inline-flex; margin-left: -11.5rem;">
-                <h3 style="font-size: 1.3rem; margin-right: 1rem;"><a href="#" style="color: green;">Advanced</a></h3>
-                <div class="btn-group mb-2 mr-2">
-                            <button type="button" class="btn btn-tertiary dropdown-toggle dropdown-toggle-split togglebtn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="fas fa-angle-down dropdown-arrow"></span>
-                                <span class="sr-only">Toggle Dropdown</span>
-                            </button>
-                            <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 44px, 0px);">
-                                <a class="dropdown-item" href="#">Edit D.O.B</a>
-                                <a class="dropdown-item" href="#">Update Contact</a>
-                                <a class="dropdown-item" href="#">Update Address</a>
-                                <a class="dropdown-item" href="#">Update Profession</a>
-                                <a class="dropdown-item" href="#">Update Gender</a>
-                                <a class="dropdown-item" href="#">Update Relationship Status</a>
-                            </div>
-                            </div>
-                </span>
-                
-                  
-                  <div class="card card-sm card-body bg-primary mb-0" style="border: 0rem; padding: 0%;">
-                        <a href="#panel-4" data-target="#panel-4" class="accordion-panel-header" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="panel-1">
-                            <span class="icon-title h6 mb-0" style="color: green; font-size: 1.3rem; margin-right: 1rem;">Advanced</span>
-                            <span class="icon" style="margin-right: 90rem;"><span class="fas fa-angle-down dropdown-arrow "></span></span>
-                        </a>
-                        <div class="collapse show" id="panel-4" style="">
-                            <div class="row pt-3">
-                              <p>
-                                <a href="#" style="margin-right: 5.7rem;">Edit D.O.B</a><br>
-                                <a href="#" style="margin-right: 3.3rem;">Update Contact</a><br>
-                                <a href="#" style="margin-right: 3.1rem;">Update Address</a><br>
-                                <a href="#" style="margin-right: 2.1rem;">Update Profession</a><br>
-                                <a href="#" style="margin-right: 3.5rem;">Update Gender</a><br>
-                                <a href="#" style="margin-left: 1.9rem;">Update Relationship Status</a>
-                              </p>
-                            </div>
-                        </div>
-                    </div>
-                  
-                </div>  </div>-->
-
   <div class="row justify-content-center mt-2" data-v-6db336fc="">
     <div class="col-12 col-md-10 col-lg-8" data-v-6db336fc="">
       <!--Accordion-->
@@ -141,7 +84,7 @@
             ><span
               class="icon-title h6 mb-0 font-weight-bold"
               data-v-6db336fc=""
-              >Change Profile Password</span
+              >Change <a style="color:green">Profile Password</a></span
             ></a
           >
           <div class="collapse" id="panel-6" data-v-6db336fc="" style="">
@@ -166,24 +109,23 @@
                   </div>
                 </div>
               </div>
-              <div class="form-group" data-v-6db336fc="">
-                <label for="exampleInputPassword1" data-v-6db336fc=""
-                  >Enter New Password</label
-                >
-                <div class="input-group mb-4" data-v-6db336fc="">
-                  <input
-                    class="form-control"
-                    id="exampleInputPassword1"
-                    placeholder="Password"
-                    type="password"
-                    aria-label="Password"
-                    data-v-6db336fc=""
-                  />
-                  <div class="input-group-append" data-v-6db336fc="">
-                    <span class="input-group-text" data-v-6db336fc=""
-                      ><span class="fas fa-unlock-alt" data-v-6db336fc=""></span
-                    ></span>
-                  </div>
+
+              <label for="exampleInputPassword1" data-v-6db336fc=""
+                >Enter New Password</label
+              >
+              <div class="input-group mb-4" data-v-6db336fc="">
+                <input
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                  type="password"
+                  aria-label="Password"
+                  data-v-6db336fc=""
+                />
+                <div class="input-group-append" data-v-6db336fc="">
+                  <span class="input-group-text" data-v-6db336fc=""
+                    ><span class="fas fa-unlock-alt" data-v-6db336fc=""></span
+                  ></span>
                 </div>
               </div>
             </div>
@@ -202,63 +144,117 @@
             ><span
               class="icon-title h6 mb-0 font-weight-bold"
               data-v-6db336fc=""
-              >Change Profile Password</span
+              >Social Links<img
+                class="ml-6 mr-2"
+                src="../assets/google.svg"
+                alt="Google"
+                width="25"
+              /><img
+                class="mr-2"
+                src="../assets/facebook.svg"
+                alt="Google"
+                width="25"
+              /><img
+                class="mr-2"
+                src="../assets/twitter.svg"
+                alt="Google"
+                width="25"
+              />
+            </span>
+          </a>
+        </div>
+        <div class="card card-sm card-body bg-primary mb-0" data-v-6db336fc="">
+          <a
+            href="#panel-8"
+            data-target="#panel-8"
+            class="accordion-panel-header collapsed"
+            data-toggle="collapse"
+            role="button"
+            aria-expanded="false"
+            aria-controls="panel-1"
+            data-v-6db336fc=""
+            ><span
+              class="icon-title h6 mb-0 font-weight-bold"
+              data-v-6db336fc=""
+              >Change Username</span
             ></a
           >
-          <div class="collapse" id="panel-7" data-v-6db336fc="" style="">
+          <div class="collapse" id="panel-8" data-v-6db336fc="" style="">
             <div class="pt-3" data-v-6db336fc="">
-              <div class="form-group" data-v-6db336fc="">
-                <label for="exampleInputPassword1" data-v-6db336fc=""
-                  >Current Password</label
-                >
-                <div class="input-group mb-4" data-v-6db336fc="">
-                  <input
-                    class="form-control"
-                    id="exampleInputPassword1"
-                    placeholder="Password"
-                    type="password"
-                    aria-label="Password"
-                    data-v-6db336fc=""
-                  />
-                  <div class="input-group-append" data-v-6db336fc="">
-                    <span class="input-group-text" data-v-6db336fc=""
-                      ><span class="fas fa-unlock-alt" data-v-6db336fc=""></span
-                    ></span>
-                  </div>
-                </div>
+              <div class="form-group focused">
+                <label for="validationServer01">New Username</label>
+                
+                
+                <div v-if="false">
+                
+                <!-- available username == true-->
+                
+
+                <input
+                  type="text"
+                  class="form-control is-valid"
+                  id="validationServer01"
+                  value="Username"
+                  required="Mark"
+                />
+                <div class="valid-feedback">
+                  Looks good!
+                </div></div>
+
+                <div v-else>
+
+
+                    <input type="text" class="form-control is-invalid" id="validationServerUsername" required="">
+                    <div class="invalid-feedback">
+                      Please choose a username.
+                    </div>               
+
+                </div> 
+
+
+
+
               </div>
-              <div class="form-group" data-v-6db336fc="">
-                <label for="exampleInputPassword1" data-v-6db336fc=""
-                  >Enter New Password</label
-                >
-                <div class="input-group mb-4" data-v-6db336fc="">
-                  <input
-                    class="form-control"
-                    id="exampleInputPassword1"
-                    placeholder="Password"
-                    type="password"
-                    aria-label="Password"
-                    data-v-6db336fc=""
-                  />
-                  <div class="input-group-append" data-v-6db336fc="">
-                    <span class="input-group-text" data-v-6db336fc=""
-                      ><span class="fas fa-unlock-alt" data-v-6db336fc=""></span
-                    ></span>
-                  </div>
-                </div>
-              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card card-sm card-body bg-primary mb-0" data-v-6db336fc="">
+          <a
+            href="#panel-9"
+            data-target="#panel-9"
+            class="accordion-panel-header collapsed"
+            data-toggle="collapse"
+            role="button"
+            aria-expanded="false"
+            aria-controls="panel-1"
+            data-v-6db336fc=""
+            ><span
+              class="icon-title h6 mb-0 font-weight-bold"
+              data-v-6db336fc=""
+              ><a style="color:green">Advanced</a
+              ><span class="icon ml-3"
+                ><span class="fas fa-plus"></span></span></span
+          ></a>
+          <div class="collapse" id="panel-9" data-v-6db336fc="" style="">
+            <div class="pt-3" data-v-6db336fc="">
+              <AdSettings />
             </div>
           </div>
         </div>
       </div>
       <!--End of Accordion-->
+      <button class="btn btn-primary text-success ml-3 mt-2" type="button"><a style='color:green'>Save</a></button>
+            <button class="btn btn-primary text-success ml-3 mt-2" type="button"><a style='color:red'>Discard</a></button>
+
     </div>
   </div>
 </template>
 
 <script>
+import AdSettings from "@/components/adset.vue";
 export default {
   name: "SettingsPage",
+  components: { AdSettings },
 };
 </script>
 
