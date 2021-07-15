8aef964657e1cd64f51cf30ac58b64ab987cdddc
chayan007 <sonicxxx7@gmail.com>
2021-07-15 18:41:01 +0530

    clean unused functions from user store

--- src/store/modules/user.store.js
+++ src/store/modules/user.store.js
@@ -7,15 +7,6 @@ export const userStore = {
         all: {}
     },
     actions: {
-        getAll({ commit }) {
-            commit('getAllRequest');
-
-            userService.getDetails()
-                .then(
-                    users => commit('getAllSuccess', users),
-                    error => commit('getAllFailure', error)
-                );
-        },
         settings({ dispatch, commit }, updateFields ) {
             commit('settingRequest', { updateFields });
 
@@ -33,25 +24,8 @@ export const userStore = {
     },
 
     mutations: {
-        getAllRequest(state) {
-            state.all = { loading: true };
-        },
-        getAllSuccess(state, users) {
-            state.all = { items: users };
-        },
-        getAllFailure(state, error) {
-            state.all = { error };
-        },
         settingRequest(updateFields){
             console.log(updateFields);
-        },
-        loginSuccess(state, user) {
-            state.status = { loggedIn: true };
-            state.user = user;
-        },
-        loginFailure(state) {
-            state.status = {};
-            state.user = null;
         },
         refreshUser(user){
             let storedProfile = JSON.parse(localStorage.getItem('user'));
