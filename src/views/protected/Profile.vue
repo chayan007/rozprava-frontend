1b6af60ff70c24236b646ca9b6356780c123ba29
itisdb <dhriteshbhagat@gmail.com>
2021-08-07 12:07:18 +0530

    profile (#24)
    * profile
    +recommendation + chat
    
    * comments are resolved
    
    * white space resolved
    
    * resolved code
    
    * arrange code formate

---
+++ src/views/protected/Profile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="box" style="text-align: center">
+    <ProfileComponent></ProfileComponent>
+    <div class="popup">
+      <BottomMenu></BottomMenu>
+    </div>
+  </div>
+</template>
+<script>
+import Profile from "@/components/profile/Profile.vue";
+import BottomMenu from "@/components/profile/BottomMenu.vue";
+
+export default {
+  name: "Profile",
+  components: { "ProfileComponent": Profile, BottomMenu },
+};
+</script>
+<style scoped>
+.popup {
+  position: fixed;
+  top: 90%;
+  left: 0%;
+  right: 0%;
+  bottom: 2%;
+  z-index: 199;
+  max-width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.box {
+  font-family: Josefin Sans;
+  margin-top: 7rem;
+  padding: 0%;
+  max-width: 100%;
+}
+</style>
