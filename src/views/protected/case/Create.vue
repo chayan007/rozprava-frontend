f723bf7419dffca05b5285af8567724803920525
Chayan <sonicxxx7@gmail.com>
2021-08-13 18:26:38 +0530

    lints

--- src/views/protected/case/Create.vue
+++ src/views/protected/case/Create.vue
@@ -1,8 +1,10 @@
 <template>
-<section class="section section bg-soft pb-5 overflow-hidden z-2">
-      <div class="row justify-content-center pt-3">
-        <div class="col-lg-8 col-xl-8">
-    <CreateForm class="mr-2 ml-2"/></div></div></section>
+  <section class="section section bg-soft pb-5 overflow-hidden z-2">
+    <div class="row justify-content-center pt-3">
+      <div class="col-lg-8 col-xl-8">
+        <CreateForm class="mr-2 ml-2"/></div>
+    </div>
+  </section>
 </template>
 
 <script>
