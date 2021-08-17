5d2ae05296df7da1eb87ebb0f5b85cd936b07cd6
SUNRIT SARKAR <81092346+Sunrit07@users.noreply.github.com>
2021-08-17 20:38:05 +0530

    Create Activity.vue
    importing Activity from components

---
+++ src/views/public/Activity.vue
@@ -0,0 +1,12 @@
+<template>
+    <Activity></Activity>
+</template>
+<script>
+// @ is an alias to /src
+import Activity from "@/components/activity/Activitypage.vue";
+
+export default {
+  name: "Activity",
+  components: {Activity},
+};
+</script>
