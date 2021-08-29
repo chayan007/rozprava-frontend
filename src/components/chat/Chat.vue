888aaac1aeb5a2ba9cc7e76bc556bf7a1e46c720
unknown <pankajbedre22@gmail.com>
2021-08-29 12:38:45 +0530

    Chat design completed

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,173 +1,185 @@
 <template>
-    <div class="a1">
-      <div class="chat-detail bg-primary shadow-soft">
-        <div>
-          <button type="button" class="a4"><img src="@/assets/chat2.png" class="back" alt="Microphone" width="25"></button>
-        </div>
-        <div class="chat-img profile-image bg-primary shadow-inset border border-light rounded-circle  ml-1">
-          <img src="@/assets/black-rose.jpg" class="card-img-top rounded-circle " alt="User image" width="100" />
-        </div>
-        <div class="friend-name"><h5>Raju Kumar</h5></div>
-      </div>
-      <div class="scroll">
-        <div class="bubble">
-          <div class="send">
-            <div class="massage">
-              ram
-            </div>
-            <div class="time-tag">
-              <span class="time">2PM</span>
-            </div>
+  <section class="chat-sec w-100">
+    <!-- chat profile -->
+    <div class="chat-pro-nav w-100 nav p-2">
+      <img class="back-arrow m-1" src="@/assets/back.svg" alt="" />
+      <a href="">
+        <img
+          class="profile-image-chat rounded-circle ml-2"
+          src="@/assets/profile-picture-1.jpg"
+          alt=""
+        />
+        <p class="list-inline-item m-0 ml-2">Shawn Carter</p>
+      </a>
+    </div>
+
+    <!-- chat box -->
+    <div class="chats-box w-100">
+      <div class="chat-scroll mt-6 m-0">
+        <!-- chat rec -->
+        <div
+          class="
+            chat-msg-box chat-msg-rec-box
+            w-100rowm-0
+            mb-3
+            justify-content-start
+          "
+        >
+          <div
+            class="chat-msg chat-msg-recive p-2 col col-lg-6 col-md-6 col-10"
+          >
+            <p class="m-0">hello hej e ejhee ehgeh</p>
+            <p class="chat-time m-0 text-right pr-1">2:39</p>
           </div>
         </div>
-        <div class="bubble">
-          <div class="receive">
-            <div class="massage">
-              ram is going
-            </div>
-            <div class="time-tag">
-              <span class="time">2PM</span>
-            </div>
+        <!-- chat send -->
+        <div
+          class="
+            chat-msg-box chat-msg-send-box
+            w-100
+            row
+            m-0
+            mb-3
+            justify-content-end
+          "
+        >
+          <div class="chat-msg chat-msg-send p-2 col col-lg-6 col-md-6 col-10">
+            <p class="m-0">
+              hello hej e ejhee ehgeh e ejeh e ejeh ehe e ehjej eje ej eje heje
+              ejehe e
+            </p>
+            <p class="chat-time m-0 text-right pr-1">2:39</p>
           </div>
         </div>
-        <div class="bubble">
-          <div class="receive">
-            <div class="massage">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem minima libero ex eaque, odio omnis officiis natus iste qui vel, impedit veritatis nesciunt obcaecati accusamus iure? Asperiores aperiam beatae nesciunt!
-              Omnis placeat a illum itaque iste veniam facere. Vitae, quidem suscipit dolorem iure provident necessitatibus assumenda dolorum alias cumque recusandae quod iste corrupti nisi impedit, sequi perferendis et amet quo.
-            </div>
-            <div class="time-tag">
-              <span class="time">2PM</span>
-            </div>
+        <!-- chat rec -->
+        <div
+          class="
+            chat-msg-box chat-msg-rec-box
+            w-100rowm-0
+            mb-3
+            justify-content-start
+          "
+        >
+          <div
+            class="chat-msg chat-msg-recive p-2 col col-lg-6 col-md-6 col-10"
+          >
+            <p class="m-0">
+              hello hej e ejhee ehgeh e ejeh e ejeh ehe e ehjej eje ej eje heje
+              ejehe e
+            </p>
+            <p class="chat-time m-0 text-right pr-1">2:39</p>
           </div>
         </div>
-      </div>
-      <div class="a2">
-        <div class="a5">
-          <input type="text"  class="a3" placeholder="Enter Message" valign="bottom">
-        </div>
-        <div class="a6">
-          <button type="button" class="a4"><img src="@/assets/attachment1.png" class="microphone" alt="Attach pic" width="25"></button>
-          <button type="button" class="a4"><img src="@/assets/send1.png" class="microphone" alt="Send Pic" width="25"></button>
-        </div>
+        
       </div>
     </div>
-</template>
-<script>
 
-  export default {
-    name: "Chat",
-  }
+    <!-- chat input -->
+    <div class="chat-input-box p-2 w-100 row m-0">
+      <div class="chat-input-outer col col-11 p-0 pr-1">
+        <div class="chat-input p-1 pl-2 col row m-0">
+          <div
+            class="chat-txt col col-12 row m-0 align-items-center"
+            placeholder="Type here..."
+            contenteditable="true"
+          ></div>
+        </div>
+      </div>
+
+      <div class="send-btn-box col col-1 p-0">
+        <img
+          class="send-btn mt-1 mb-1 text-right"
+          src="@/assets/send.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+};
 </script>
+
 <style scoped>
-.bubble{
-  display: flex;
-  flex-direction: column;
+.chat-sec {
+  position: absolute;
+  top: 0vh;
+  height: 100vh;
+  background-color: rgb(231, 231, 231);
 }
-.time{
-  font-size: 10pt;
-  opacity: 0.7;
+.chat-pro-nav {
+  background-color: white;
+  position: fixed;
+  z-index: 20;
 }
-.time-tag{
-  margin-left: auto;
-  margin-right: 0rem;
-  display: inline-block;
+.chat-pro-nav a {
+  display: inline-flex;
+  align-items: center;
 }
-.massage{
-  padding: 1rem 1rem 0rem 1rem;
+.back-arrow {
+  width: 1.2em;
 }
-.back{
-  margin-top: 0.5rem;
-  background: transparent;
+.profile-image-chat {
+  width: 2.2em;
 }
-.scroll{
-  overflow-y: auto;
+/* chats-box */
+.chats-box {
+  position: fixed;
+  top: 0;
+  height: 100vh;
+  overflow-y: scroll;
+  padding: 0.5em;
 }
-.scroll::-webkit-scrollbar{
+.chat-msg {
+  border-radius: 15px;
+}
+.chat-time {
+  font-size: 0.79em;
+}
+.chat-msg-recive {
+  background-color: white;
+}
+.chat-msg-send {
+  background-color: #5458f7;
+  color: white;
+}
+/* Chat input */
+.chat-input-box {
+  position: fixed;
+  bottom: 0;
+  z-index: 50;
+  background-color: white;
+}
+.chat-input-outer {
+  display: inline-flex;
+  align-items: stretch;
+}
+.chat-input {
+  background-color: rgb(231, 231, 231);
+  border-radius: 25px;
+}
+.chat-txt {
+  border: none;
   text-decoration: none;
 }
-.send-box{
-  padding: 1rem;
+[contenteditable] {
+  outline: 0px solid transparent;
 }
-.send{
-  max-width: 70%;
-  border-radius: 0.5rem;
-  background-color: rgb(167, 245, 167);
-  margin-right: 0%;
-  margin-left: auto;
-  margin-bottom: 0.5rem;
-  display: inline-flex;
-  flex-direction: column;
+[placeholder]:empty::before {
+  content: attr(placeholder);
+  color: #555;
 }
-.receive{
-  max-width: 70%;
-  border-radius: 0.5rem;
-  background-color: white;
-  margin-left: 2%;
-  margin-right: auto;
-  margin-bottom: 0.5rem;
-  display: inline-flex;
-  flex-direction: column;
+[placeholder]:empty:focus::before {
+  content: "";
 }
-.a6{
-  white-space: nowrap;
-  margin-right: 1rem !important;
-}
-.a5{
-  width: 100%;
-}
-.a4{
-  border: none;
-  background: none;
-}
-.a3{
-  outline: none;
-  border: none;
-  border-radius: 1rem;
-  background-color: rgb(215, 247, 247);
-  padding: 0.3rem 1rem;
-  margin-left: 0rem;
-  width: 100%;
-}
-.friend-name{
-  margin-top: 1rem;
-  margin-left: 1rem;
-}
-.chat-detail{
-  display: flex;
-  z-index: 200;
-}
-.a2{
-  background-color: white;
-  width: 100%;
-  margin-top: auto;
-  margin-bottom: 0rem;
-  padding: 0.3rem;
+.send-btn-box {
   display: flex;
 }
-.a1{
-  width: 50%;
-  margin-left: 25%;
-  margin-right: auto;
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  bottom: 0rem;
-  top: 6.2rem;
-}
-@media (max-width: 700px) {
-  .a1 {
-    width: 100%;
-    top: 6rem;
-    margin-left: 0%;
-  }
-}
-@media (max-width: 700px) {
-  .send{
-    max-width: 80%;
-  }
-  .receive{
-    max-width: 80%;
-  }
+.send-btn {
+  width: 2em;
+  padding-left: 2px;
 }
 </style>
